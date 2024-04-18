62b9ae182182307c513fd12926e23000fe51198b
Anaïs Trinquet <115187313+KoffeeKoffee@users.noreply.github.com>
2024-04-18 16:20:36 +0200

    creature script

--- TowerDefense/Assets/Scripts/Creature.cs
+++ TowerDefense/Assets/Scripts/Creature.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Creature : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavMeshAgent _navMeshAgent;
+    private CreatureSO _data;
+    private int _currentHealth;
+    public Action<float> OnDamageTaken;
+    public Action<Creature> OnCreatureEliminated;
+    public Action<Creature> OnCreatureReachedEnd;
+
+    private void Awake()
     {
-        
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        OnCreatureEliminated += Game.Instance.HandleCreatureEliminated;
+        OnCreatureReachedEnd += Game.Instance.HandleCreatureReachedEnd;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        OnDamageTaken?.Invoke(_currentHealth / (float)_data.health);
+        if (_currentHealth <= 0)
+        {
+            OnCreatureEliminated?.Invoke(this);
+        }
+    }
+
+    public void MoveTo(Vector3 target)
+    {
+        _navMeshAgent.destination = target;
     }
 }
--- TowerDefense/Assets/Scripts/CreatureSO.cs
+++ TowerDefense/Assets/Scripts/CreatureSO.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "CreatureSO", menuName = "CreatureSO", order = 0)]
 public class CreatureSO : ScriptableObject
 {
+    public int health;
+    public float speed;
+    public int score;
+    public int reward;
 }
