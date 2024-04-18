bba365b68b4e23ed9f5aab0ea681c2ed020a6a31
Anaïs Trinquet <115187313+KoffeeKoffee@users.noreply.github.com>
2024-04-18 15:47:55 +0200

    creature manager

--- TowerDefense/Assets/Scripts/CreatureManager.cs
+++ TowerDefense/Assets/Scripts/CreatureManager.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class CreatureManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private ObjectPool<Creature> _creaturePool;
+    public Action<Creature> OnCreatureSpawned;
 
-    // Update is called once per frame
-    void Update()
+    private Creature SpawnCreature(CreatureSO creatureData, Vector3 position)
     {
-        
+        Creature creature = _creaturePool.Get();
+        //TODO
+        return creature;
     }
 }
