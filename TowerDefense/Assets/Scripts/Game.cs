e5fc83bac3e94737da675b324c29fc09940b9f82
Anaïs Trinquet <115187313+KoffeeKoffee@users.noreply.github.com>
2024-04-18 15:05:54 +0200

    variables game

--- TowerDefense/Assets/Scripts/Game.cs
+++ TowerDefense/Assets/Scripts/Game.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,25 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     private Arena _arena;
+    private WaveManager _waveManager;
+    private TowerManager _towerManager;
+    private UIManager _uiManager;
+    private CreatureManager _creatureManager;
+    private int _score;
+    private int _multiplier;
+    private int _currency;
+
+    public Action<int> OnScoreUpdated;
+    public Action<int> OnMultiplierUpdated;
+    public Action<int> OnCurrencyUpdated;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
