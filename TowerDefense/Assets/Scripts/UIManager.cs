986183b36026db08eb5e31420759ce9e2a52891c
Anaïs Trinquet <115187313+KoffeeKoffee@users.noreply.github.com>
2024-04-18 15:47:46 +0200

    ui manager

--- TowerDefense/Assets/Scripts/UIManager.cs
+++ TowerDefense/Assets/Scripts/UIManager.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI multiplierText;
+    [SerializeField] private TextMeshProUGUI currencyText;
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private TextMeshProUGUI remainingCreatureText;
+
+    private void Start()
     {
-        
+        Game.Instance.OnCurrencyUpdated += (currency) => UpdateText(currencyText, currency);
+        Game.Instance.OnScoreUpdated += (score) => UpdateText(scoreText, score);
+        Game.Instance.OnMultiplierUpdated += (multiplier) => UpdateText(multiplierText, multiplier);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateText(TextMeshProUGUI text, int value)
     {
-        
+        text.text = "" + value;
     }
 }
