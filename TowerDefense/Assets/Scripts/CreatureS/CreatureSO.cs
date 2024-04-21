a17832041dcc90e527ef8bc3b2f49170fb51c028
Anaïs Trinquet <115187313+KoffeeKoffee@users.noreply.github.com>
2024-04-21 15:57:49 +0200

    wave ui update

--- TowerDefense/Assets/Scripts/CreatureS/CreatureSO.cs
+++ TowerDefense/Assets/Scripts/CreatureS/CreatureSO.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CreatureSO", menuName = "CreatureSO", order = 0)]
 public class CreatureSO : ScriptableObject
 {
+    public GameObject prefab;
     public int health;
     public float speed;
     public int score;
