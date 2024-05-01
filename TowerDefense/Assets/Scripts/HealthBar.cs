caf10fe2785023ea3901cd64c729b7b09b7a7b2c
Anaïs Trinquet <115187313+KoffeeKoffee@users.noreply.github.com>
2024-05-01 17:19:30 +0200

    look at health bar

--- TowerDefense/Assets/Scripts/HealthBar.cs
+++ TowerDefense/Assets/Scripts/HealthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,24 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private RectTransform fill;
     private Image _fillImage;
+    private Camera _camera;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.parent.GetComponent<LivingEntity>().OnDamageTaken += UpdateHealth;
         _fillImage = fill.GetComponent<Image>();
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        gameObject.transform.LookAt(_camera.transform);
     }
 
     private void UpdateHealth(float normalizedHealth)
     {
-        fill.anchorMax = new(fill.anchorMax.x, normalizedHealth);
+        fill.anchorMax = new(normalizedHealth, fill.anchorMax.y);
         _fillImage.color = Color.Lerp(Color.red, Color.green, normalizedHealth);
     }
 }
