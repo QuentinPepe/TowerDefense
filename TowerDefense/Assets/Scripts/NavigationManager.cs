3c42b207b0abf4d65cd1166240b3f88a00885b7c
Anaïs Trinquet <115187313+KoffeeKoffee@users.noreply.github.com>
2024-05-06 17:32:28 +0200

    main menu

---
+++ TowerDefense/Assets/Scripts/NavigationManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigationManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("_Main");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
