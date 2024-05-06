02f5cc934af5c5bde86b5af3794470e6546d838d
Quentin PEPE <quentin.pepe@ensiie.eu>
2024-05-06 17:26:02 +0200

    Add music slider

---
+++ TowerDefense/Assets/Scripts/OptionVolume.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class OptionVolume : MonoBehaviour
+{
+    [SerializeField] private List<Slider> sliders;
+    [SerializeField] private List<string> groupNames;
+    [SerializeField] private AudioMixer audioMixer;
+    private void Start()
+    {
+        for (int i = 0; i < sliders.Count; i++)
+        {
+            int index = i;
+            sliders[i].onValueChanged.AddListener(value => HandleSliderValueChanged(value, groupNames[index]));
+        }
+
+        for (int i = 0; i < sliders.Count; i++)
+        {
+            sliders[i].value = audioMixer.GetFloat(groupNames[i], out float value) ? value : 0;
+        }
+    }
+
+    private void HandleSliderValueChanged(float value, string groupName)
+    {
+        audioMixer.SetFloat(groupName, value);
+    }
+}
